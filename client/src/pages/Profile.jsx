56fcd48647eb40641885f3aeb524b14895003316
winglaw12 <winghlaw@gmail.com>
2024-10-26 19:57:41 -0700

    add signout functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux'
 import { useRef, useState, useEffect } from 'react'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from '../firebase'
-import {  updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice'
+import {  deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
 
 export default function Profile() {
@@ -78,6 +78,38 @@ const handleSubmit = async (e) => {
   }
 };
 
+const handleDeleteUser = async()=> {
+  try {
+    dispatch(deleteUserStart())
+    const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+      method: 'DELETE',
+    })
+    const data = res.json()
+    if (data.success === false) {
+      dispatch(deleteUserFailure(data))
+      return 
+    }
+    dispatch(deleteUserSuccess(data))
+  } catch (error) {
+    dispatch(deleteUserFailure(error.message))
+  }
+}
+
+const handleSignOut = async () => {
+  try {
+    dispatch(signOutUserStart());
+    const res = await fetch('/api/auth/signout');
+    const data = await res.json();
+    if (data.success === false) {
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data));
+  } catch (error) {
+    dispatch(deleteUserFailure(data.message));
+  }
+}
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -137,12 +169,14 @@ const handleSubmit = async (e) => {
 
       <div className='flex justify-between mt-5'>
         <span
-          // onClick={handleDeleteUser}
+          onClick={handleDeleteUser}
           className='text-red-700 cursor-pointer'
         >
           Delete account
         </span>
-        <span className='text-red-700 cursor-pointer'>
+        <span 
+          onClick={handleSignOut} 
+          className='text-red-700 cursor-pointer'>
           Sign out
         </span>
       </div>
